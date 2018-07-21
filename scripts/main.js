5f11c0010ce15fc0463f8d76b6f32ddde1722de8
guowy433110 <1540778560@qq.com>
2018-07-21 12:24:43 +0800

    added everything

---
+++ scripts/main.js
@@ -0,0 +1,10 @@
+function output(){
+	$('formid').submit(function(){
+		// var password1 = $('exampleInputPassword1').val();
+		// alert(password1);
+		var email = $('exampleInputEmail1').val();
+		alert(email);
+	})
+}
+
+output();
